3a9306ac3ecaa2c8874336c9806de7f6d2742e44
master-dhanesh <dhanesh1296@gmail.com>
2024-09-19 18:21:27 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+    // javascript logic
+
+    return (
+        <div>
+            <div>
+                {/* always return HTML */}
+                <h1>Hello</h1>
+            </div>
+            <div>
+                <h1>Hiii</h1>
+            </div>
+        </div>
+    );
+};
+
+export default App;
